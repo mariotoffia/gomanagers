c4e480d89bee8f16777260acc6f04b571e69d616
Mario Toffia <mario.toffia@gmail.com>
2021-04-29 08:39:35 +0200

    renamed to goservice

--- interfaces/ifkms/kms.go
+++ interfaces/ifkms/kms.go
@@ -1,11 +1,11 @@
 package ifkms
 
 import (
-	"github.com/mariotoffia/gomanagers/interfaces/ifcrypto"
-	"github.com/mariotoffia/gomanagers/model/coremodel"
+	"github.com/mariotoffia/goservice/interfaces/ifcrypto"
+	"github.com/mariotoffia/goservice/model/coremodel"
 )
 
-// Signer is a entity that may sign or verify a signature.
+// Signer is a entity that may sign a signature.
 type Signer interface {
 	// Sign will sign the _msg_ using the provided _key_.
 	Sign(
@@ -14,7 +14,10 @@ type Signer interface {
 		signAlgorithm ifcrypto.SignAlgorithm,
 		tags ...coremodel.Meta,
 	)
+}
 
+// Verifier is implemented by those who may verify a signature.
+type Verifier interface {
 	// Verify will verify the _msg_ using the provided _key_
 	Verify(
 		msg []byte,
